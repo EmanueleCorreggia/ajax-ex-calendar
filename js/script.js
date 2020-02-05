1e8f926a11ceea726d0ff201eb987d2f91cc6d04
Emanuele <emanuele.correggia86@gmail.com>
2020-02-05 02:38:53 +0100

    correzioni

--- js/script.js
+++ js/script.js
@@ -11,9 +11,6 @@ $(document).ready(function () {
     }
   );
 
-  // console.log(baseMonth.format('MMMM'));
-  // console.log(baseMonth.format('YYYY-MM'));
-
 
   printMonth(baseMonth);
   printHoliday(baseMonth);
@@ -21,8 +18,7 @@ $(document).ready(function () {
   // quando clicchiamo su next
   $('#next').click(function () {
     //dobbiamo andare avanti di un mese e chiamare la funzione che genera i guiorni e poi chiamare quella che crea le festivita
-    // $('h1').attr('data-this-month', 'devo mettere un valore')
-    // console.log($('h1').attr('data-this-month'));
+
     var thisMonth = $('h1').attr('data-this-month');
     var date = moment(thisMonth).add(1, 'months');
     console.log(date);
@@ -59,8 +55,6 @@ function printMonth(month) {
 
   // faccio un ciclo che parte da 1 fino a 31 incluso
   for (var i = 1; i <= daysInMonth ; i++) {
-    // console.log(i);
-    // "2018-01-06"
 
     var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
@@ -83,8 +77,7 @@ function addZero(num) {
 
 
 function printHoliday(month) {
-  // console.log(month.month());
-  // console.log(month.year());
+
   $.ajax(
     {
       url: 'https://flynn.boolean.careers/exercises/api/holidays',
@@ -94,31 +87,21 @@ function printHoliday(month) {
         month: month.month()
       },
       success: function (data) {
-        // console.log(data.response);
+
         var holidays = data.response;
         //prendo ogni holidays e la confronto con ogni li sul dom se quel li contiene la stessa data allora la coloro di rosso
 
         //ciclo sugli elementi di holidays
         for (var i = 0; i < holidays.length; i++) {
           var thisHoliday = holidays[i];
-          // console.log(thisHoliday);
+
           var thisHolidayData = thisHoliday.date;
-          // console.log(thisHolidayData);
-          //confrontare questa data con tutte le date negli li
-          //ciclo sgli li usando jquery
-          // $('.day').each(function () {
-          //   var elementDate = $(this).attr('data-date-complete');
-          //   // console.log(elementDate);
-          //   if(thisHolidayData == elementDate) {
-          //     $(this).addClass('red');
-          //     $(this).find('.nome-festivita').append(thisHoliday.name);
-          //   }
-          // });
+
 
           //metodo con data attr
           $('li[data-date-complete="'+ thisHolidayData  +'"]').addClass('red');
           $('li[data-date-complete="'+ thisHolidayData  +'"]').find('.nome-festivita').append(thisHoliday.name);
-          // 'li[data-date-complete="2018-01-01"]'
+
         }
       },
       error: function () {
@@ -127,17 +110,3 @@ function printHoliday(month) {
     }
   );
 }
-
-// {
-//     "success": true,
-//     "response": [
-//         {
-//             "name": "Capodanno",
-//             "date": "2018-01-01"
-//         },
-//         {
-//             "name": "Epifania",
-//             "date": "2018-01-06"
-//         }
-//     ]
-// }
